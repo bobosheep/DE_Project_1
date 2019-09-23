3954a0049e588a33f9656504358541b30e12e7c6
Ian Tsai <ianpaul32@gmail.com>
2019-09-23 14:19:45 +0800

    rename & update

---
+++ split_articles.py
@@ -0,0 +1,49 @@
+import sys
+import time
+
+NEWS_DATA = './news_data/ettoday.rec'
+Processed_data = './news_data/processed_data.rec'
+paragraphList = []
+
+start = time.clock()
+
+with open(NEWS_DATA, 'r', encoding='utf-8', errors='ignore') as fp:
+    inbody = False
+    paragraph = ''          #get the info of @body
+    article_count = 0
+    for line in fp:
+
+        if inbody :
+            if line.startswith('@') :       #the end of the body
+                inbody = False
+                #print('finish')
+                paragraphList += [paragraph]
+            paragraph = paragraph + line
+
+        if line.startswith('@body') :
+            inbody = True
+            paragraph = line.split(':')[1]
+            article_count += 1
+
+end = time.clock()
+
+print('len of paragraphList: ' + str(len(paragraphList)))
+totalSize = 0
+for p in paragraphList:
+    totalSize += sys.getsizeof(p)
+    #print(str(sys.getsizeof(p)) + '  ' + p)
+
+mem_cost = sys.getsizeof(paragraphList)
+time_cost = end - start
+
+with open(Processed_data, 'w', encoding='utf-8') as fp:
+    fp.writelines(paragraphList)
+
+print('mem cost = ' + str(mem_cost) + ', total size = ' + str(totalSize) )
+
+with open('split_articles.info', 'w') as fp:
+    fp.write('Total articles: ' + str(article_count) + '\n')
+    fp.write('Time cost: ' + str(time_cost) + ' s\n')
+    fp.write('Memory cost: ' + str(mem_cost) + ' bytes\n')
+    fp.write('Average time cost per article: ' + str(time_cost / article_count * 1000) + ' ms\n')
+    fp.write('Average memory cost per article: ' + str(mem_cost / article_count) + ' bytes\n')
--- split_sentences.py
+++ split_sentences.py
@@ -1,35 +1,22 @@
 import re
 import hashlib
+import sys
+import time
 
 md5 = hashlib.md5()
 break_data = './news_data/processed_data.rec'
-saved_data = './news_data/sentences_{}.rec'
 sentences = {}
 lines_count = 0
+
+
+start = time.clock()
 
 with open(break_data, 'r', encoding='utf-8', errors='replace') as fp:
     file_count = 0
     for line in fp:
         
-        '''
-        if(count == 1000):
-            file_count += 1
-            count = 0
-        with open(saved_data.format(file_count), 'a+', encoding='utf-8') as savedfp:    
-        '''
         sentence = re.split(r'[\?\!\！\？\。\;\；\|\｜\，\：][\」\》\）\)]?', line)
-        
-        
         sentence_length = len(sentence)
-
-        '''
-        for i in range(0, sentence_length - 1, 2):
-            sentence[i] = sentence[i] + sentence[i + 1]
-
-        if lines_count < 5:
-            print('after:')
-            print(sentence)
-        '''
 
         for s in sentence:
             
@@ -50,32 +37,34 @@ with open(break_data, 'r', encoding='utf-8', errors='replace') as fp:
                s.startswith('；') or s.startswith(';') :
                 continue
 
-            #md5.update(s.encode('utf-8'))
-            #encode_s = md5.hexdigest()
-
             if sentences.get(s) is None:
                 sentences[s] = 1
             else:
                 sentences[s] += 1
 
-            #print(s)
-            #sentences += s
-            #savedfp.write(s)
 
-            if lines_count < 10:
-                print(s)
 
 
         lines_count += 1
-        if lines_count % 5000 == 0:
-            print('Processed {} datas'.format(lines_count))
+        #if lines_count % 5000 == 0: 
+            #print('Processed {} datas'.format(lines_count))
             #print(sentences)
+
+end = time.clock()
+
+mem_cost = sys.getsizeof(sentences)
+split_time_cost = end - start
+sentence_count = len(sentences)
 
 print('finish break sentence')
 print("total {} lines".format(lines_count))
-print("total {} sentences".format(len(sentences)))
+print("total {} sentences".format(sentence_count))
 
+start = time.clock()
 sentences = [ v for v in sorted(sentences.items(), key = lambda x : x[1], reverse=True)]
+end = time.clock()
+
+sort_time_cost = end - start
 
 items = sentences
 count = 0
@@ -89,6 +78,16 @@ with open('./news_data/sentences.rec', 'w', encoding='utf-8') as fp:
     for sentence_result in sentences:
         fp.write(sentence_result[0] + ': ' + str(sentence_result[1]))
         fp.write('\n')
+
+
+with open('split_articles.info', 'w') as fp:
+    fp.write('Total sentences: ' + str(sentence_count) + '\n')
+    fp.write('Split time cost: ' + str(split_time_cost) + ' s\n')
+    fp.write('Sort time cost: ' + str(sort_time_cost) + ' s\n')
+    fp.write('Memory cost: ' + str(mem_cost) + ' bytes\n')
+    fp.write('Average split time cost per sentence: ' + str(split_time_cost / sentence_count * 1000) + ' ms\n')
+    fp.write('Average sort time cost per sentence: ' + str(sort_time_cost / sentence_count * 1000) + ' ms\n')
+    fp.write('Average memory cost per sentence: ' + str(mem_cost / sentence_count) + ' bytes\n')
 
 #sentences.sort()
 #print(sentences)
