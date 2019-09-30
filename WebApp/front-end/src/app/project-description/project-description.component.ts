271595903e8dc20c5ceefe9384588f8c7bf30b26
Ian Tsai <ianpaul32@gmail.com>
2019-10-01 02:03:49 +0800

    version 0.0.2

--- WebApp/front-end/src/app/project-description/project-description.component.ts
+++ WebApp/front-end/src/app/project-description/project-description.component.ts
@@ -7,6 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProjectDescriptionComponent implements OnInit {
 
+  projects: any[] = [{
+    title : 'Proj.1 斷句與搜尋',
+    subtitle : 'Breaking Sentences and Searching', 
+    content : '根據提供之新聞資料（約1.9 GB）將所有文章內容做斷句，\
+               並將其排序、計算句子重複次數，最後以網頁型式呈現，並提供簡單搜尋功能。',
+    links: [{url:'/search', text:'Go Search'}, {url:'/performance', text:'Performance'},
+            {url:'/all-sentence', text:'Sentences'}] 
+  }]
+
   constructor() { }
 
   ngOnInit() {
